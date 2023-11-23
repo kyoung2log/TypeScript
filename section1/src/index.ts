9829404058772e2156189a656643775a5384c1f2
chestnut1044 <wbk1620@gmail.com>
2023-11-23 20:23:28 +0900

    ts 기초

---
+++ section1/src/index.ts
@@ -0,0 +1,10 @@
+console.log('hello ts');
+const a: number = 1;
+console.log(a);
+
+// const func = () => console.log('test');
+
+// import { hello } from './hello';
+// hello('ww');
+
+// const a = 1;
