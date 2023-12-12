c132d2468790066fef75931a4f082ea59ad6cd2d
chestnut1044 <wbk1620@gmail.com>
2023-12-12 15:26:05 +0900

    docs : void와 never

---
+++ section2/src/chapter7.ts
@@ -0,0 +1,17 @@
+function func1(): string {
+  return 'hello';
+}
+
+// void : 아무것도 없음을 의미하는 타입
+function func2(): void {
+  console.log('hello');
+}
+
+// never : 존재하지 않는 불가능한 타입
+function func3(): never {
+  while (true) {}
+}
+
+function func4(): never {
+  throw new Error();
+}
