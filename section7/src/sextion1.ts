5fb599e2dcdc7e31507fb62de4bf0e054efe120a
chestnut1044 <wbk1620@gmail.com>
2023-12-14 16:47:51 +0900

    docs : init/제네릭

---
+++ section7/src/sextion1.ts
@@ -0,0 +1,15 @@
+/**
+ * 제네릭
+ */
+
+// 제네릭 함수
+// <T> 타입 변수
+function func<T>(value: T): T {
+  return value;
+}
+
+let num = func(10);
+let bool = func(true);
+let str = func('string');
+
+let arr = func<[number, number, number]>([1, 2, 3]);
