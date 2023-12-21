31dd87e5cc9a43756063698498ced5a900f78a31
chestnut1044 <wbk1620@gmail.com>
2023-12-21 18:55:33 +0900

    feat : 입력 컴포넌트 구현

---
+++ section11/src/components/Editor.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+interface Props {
+  onClickAdd: (text: string) => void;
+}
+
+export default function Editor(props: Props) {
+  // useState라는 메서드에 초기값으로 전달한 인수에 따라
+  // state 변수와 state 변화함수의 타입을 자동으로 추론해준다.
+  // useState같은 함수를 제네릭 함수라고 한다.
+  const [text, setText] = useState('');
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const onClickButton = () => {
+    props.onClickAdd(text);
+    setText('');
+  };
+
+  return (
+    <div>
+      <input value={text} onChange={onChangeInput}></input>
+      <button onClick={onClickButton}>추가</button>
+    </div>
+  );
+}
